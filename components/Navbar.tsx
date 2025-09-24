7ca7b414f20d311d9e81f22e2763438c72ae66d8
scriptspectra <scriptspectra@gmail.com>
2025-09-24 22:56:06 +0530

    Added the main structure

---
+++ components/Navbar.tsx
@@ -0,0 +1,55 @@
+import { cn } from '@/lib/utils'
+import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from '@clerk/nextjs'
+import { Sparkles } from 'lucide-react'
+import { Poppins } from 'next/font/google'
+import Link from 'next/link'
+import React from 'react'
+import { Button } from './ui/button'
+import MobileSidebar from './MobileSidebar'
+import ModeToggle from './mode-toggle'
+
+const font = Poppins({
+    weight: "600",
+    subsets: ["latin",]
+})
+
+const Navbar = () => {
+  return (
+    <div className='text-black w-full fixed z-50 flex items-center border-b-1 justify-between border-primary/10  pl-5 pr-5 bg-slate-200 mt-0 dark:bg-[#2A2A2A] dark:text-white h-16'>
+        <div className='flex items-center'>
+            <div className='block md:hidden text-black dark:text-slate-200'>
+                <MobileSidebar />
+            </div>
+            <Link href={'/'}>
+                <h1
+                 className={cn('hidden md:block text-xl font-bold text-black md:text-3xl dark:text-[#CD6E51]',
+                            font.className
+                )}>
+                    Xplora
+                </h1>
+            </Link>
+        </div>
+        <div className='flex gap-3'>
+            <SignedIn>
+                <ModeToggle />
+                <div className="dark">
+                    <UserButton
+                        appearance={{
+                            baseTheme: 'dark',
+                            elements: {
+                            userButton: 'bg-gray-900 text-white hover:bg-gray-800',
+                            },
+                        }}
+                    />
+                </div>
+           </SignedIn>
+            <SignedOut>
+                <SignInButton />
+                <SignOutButton />
+            </SignedOut>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
